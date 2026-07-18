d6d4eb315ccfee9415ef2aa8d48f0eb87a914a07
h41tam <hw.businessonly@gmail.com>
2026-07-18 19:23:10 +0100

    chore: add Netlify deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,6 +13,7 @@ const config = defineConfig({
     devtools(), 
     tailwindcss(), 
     tanstackStart(), 
+    netlify(),
     viteReact()
   ],
 })
